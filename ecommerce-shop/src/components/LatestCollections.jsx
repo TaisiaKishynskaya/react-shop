db956c7234caa69d2b95f22d60d00d82a2d6a269
Taisiia Kyshynska <tayusha201449@gmail.com>
2024-10-24 14:41:13 +0200

    Update header section, output latest products array to console

---
+++ ecommerce-shop/src/components/LatestCollections.jsx
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react'
+import { ShopContext } from '../context/ShopContext'
+
+const LatestCollections = () => {
+    const {products} = useContext(ShopContext);
+    
+    console.log(products);
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default LatestCollections
--- ecommerce-shop/src/pages/Home.jsx
+++ ecommerce-shop/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react"
 import Hero from "../components/Hero"
+import LatestCollections from "../components/LatestCollections"
 
 const Home = () => {
   return (
     <>
       <div>
         <Hero />
+        <LatestCollections />
       </div>
     </>
   )
