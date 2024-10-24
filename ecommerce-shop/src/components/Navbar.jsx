9a2bc80b0b1b589c5c593c4260400f0d0b737665
Taisiia Kyshynska <tayusha201449@gmail.com>
2024-10-24 15:17:08 +0200

    Add latest products section

--- ecommerce-shop/src/components/Navbar.jsx
+++ ecommerce-shop/src/components/Navbar.jsx
@@ -9,7 +9,9 @@ const Navbar = () => {
     return (
         <div className='flex items-center justify-between py-5 font-medium'>
 
+        <Link to='/'>
             <img src={assets.logo} className='w-36' alt="" />
+        </Link>
 
             {/*Основное меню - для больших экранов*/}
             <ul className='hidden  sm:flex gap-5 text-sm text-gray-700'>
