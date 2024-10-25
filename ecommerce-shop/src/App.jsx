beb02e5b7aacc3b34fa95c4aa72288a3ea7937f9
Taisiia Kyshynska <tayusha201449@gmail.com>
2024-10-25 09:47:00 +0200

    Add Search feature

--- ecommerce-shop/src/App.jsx
+++ ecommerce-shop/src/App.jsx
@@ -11,12 +11,14 @@ import Orders from "./pages/Orders"
 import Navbar from "./components/Navbar"
 import Cart from "./pages/Cart"
 import Footer from "./components/Footer"
+import SearchBar from "./components/SearchBar"
 
 const App = () => {
   return (
     <>
       <div className='px-4 sm:px-[7vw] lg:px-[9vw]'>
         <Navbar />
+        <SearchBar/>
         <Routes>
           <Route path='/' element={<Home/>} />
           <Route path='/collection' element={<Collection/>} />
--- ecommerce-shop/src/pages/Collection.jsx
+++ ecommerce-shop/src/pages/Collection.jsx
@@ -5,7 +5,7 @@ import Title from '../components/Title';
 import ProductItem from '../components/ProductItem';
 
 const Collection = () => {
-  const {products} = useContext(ShopContext);
+  const {products, search, showSearch} = useContext(ShopContext);
   const [showFilter, setShowFilter] = useState(false); // когда маленький экран - false скрывает выпадающие фильтры 
   const [filterProducts, setFilerProducts] = useState([]);
   const [category, setCategory] = useState([]);
@@ -32,6 +32,10 @@ const Collection = () => {
   const applyFilter = () =>{
     let productsCopy = products.slice();
 
+    if(showSearch && search){
+      productsCopy = productsCopy.filter(item => item.name.toLowetCase().includes(search.toLowetCase()));
+    }
+
     if (category.length > 0 ){
       productsCopy = productsCopy.filter(item=>category.includes(item.category));
     }
@@ -45,7 +49,7 @@ const Collection = () => {
 
   useEffect(()=>{
     applyFilter();
-  },[category,setCategory])  
+  },[category,setCategory, search, showSearch])  
 
   return (
     <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
